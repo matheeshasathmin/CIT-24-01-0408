05351f94d41a8b3be2c2b4d57f45ab8db265fd50
Matheesha Sathmin <cit-24-01-0408@sltc.ac.lk>
2026-07-01 20:35:39 +0530

    Fixed start-app.sh to handle port conflicts

--- start-app.sh
+++ start-app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Running app..."
+sudo fuser -k 5000/tcp 2>/dev/null
+docker rm -f $(docker ps -aq) 2>/dev/null
 docker compose up -d
 echo ""
 echo "The app is available at http://localhost:5000"
